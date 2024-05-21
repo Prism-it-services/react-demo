64ff89d98f0daeeb48861e0631010e630d7b6b3d
Sangeetha <sangeetha@RRs-MacBook-Air.local>
2024-05-21 16:16:38 +0100

    Route 53, https SSL encryption and changes to code pipeline

--- src/components/TechnicalSkills/styles.tsx
+++ src/components/TechnicalSkills/styles.tsx
@@ -3,29 +3,63 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 20px;
-  max-width: 800px;
-  margin: auto;
 `;
 
-export const Title = styled.h1`
-  text-align: center;
-  margin-bottom: 40px;
-`;
-
-export const Section = styled.div`
-  margin-bottom: 20px;
+export const Header = styled.h1`
+  font-size: 2rem;
+  color: #333;
 `;
 
 export const SectionTitle = styled.h2`
-  margin-bottom: 10px;
-  color: #2c3e50;
+  font-size: 1.5rem;
+  color: #333;
+  margin-top: 20px;
 `;
 
-export const List = styled.ul`
-  list-style-type: none;
+export const SkillsList = styled.ul`
+  list-style: none;
   padding: 0;
 `;
 
-export const ListItem = styled.li`
-  margin-bottom: 5px;
+export const SkillItem = styled.li`
+  margin: 10px 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const CheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+  font-size: 1rem;
+  color: #333;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const Checkbox = styled.input`
+  margin-left: 5px;
+`;
+
+export const SkillText = styled.span`
+margin-right: 10px;
+text-align: right; 
+`;
+
+
+export const SubSkillsList = styled.ul`
+  list-style: none;
+  padding: 0;
+  padding-left: 20px;  /* indent sub-skills */
+`;
+
+export const SubSkillItem = styled.li`
+  margin: 5px 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const SubSkillText = styled.span`
+  margin-right: 10px;
 `;
