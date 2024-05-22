eb41470602e9dfa48f32b6af1addbd280dfe704f
Sangeetha <sangeetha@RRs-MacBook-Air.local>
2024-05-22 11:36:34 +0100

    minor changes to blog

--- src/components/MyBlogs/MyBlogs.tsx
+++ src/components/MyBlogs/MyBlogs.tsx
@@ -11,6 +11,8 @@ return (
     <li>how to consolidate package.json to a single file when you have appsync and react app in a single project</li>
     <li>What is cdk.json?</li>
     <li>tsconfig.json - Compiler options</li>
+    <li>React Rendering concepts (Mark Erikson blog)</li>
+    <li>State Management blog (uesState vs props vs flux implementation)</li>
 </ul>
 </>
 )};
